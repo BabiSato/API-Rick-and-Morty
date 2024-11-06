3354ff69f51d4297fb87d0289ee7bec13fe96a77
BabiSato <barbaranayumisato@gmail.com>
2024-11-06 18:56:21 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,28 @@
+const characterContainer = document.getElementById('characterContainer');
+
+async function fetchCharacters() {
+    try {
+        const response = await fetch('https://rickandmortyapi.com/api/character');
+        const data = await response.json();
+        displayCharacters(data.results);
+    } catch (error) {
+        console.error("Erro ao buscar personagens:", error);
+    }
+}
+
+function displayCharacters(characters) {
+    characterContainer.innerHTML = '';
+    characters.forEach(character => {
+        const characterDiv = document.createElement('div');
+        characterDiv.className = 'character';
+        characterDiv.innerHTML = `
+            <h3>${character.name}</h3>
+            <img src="${character.image}" alt="${character.name}" width="150">
+            <p>Espécie: ${character.species}</p>
+            <p>Status: ${character.status}</p>
+        `;
+        characterContainer.appendChild(characterDiv);
+    });
+}
+
+fetchCharacters();
